2acd7870ba899fc26ddc8d6382620fd94d3b6762
flak15 <flak160@gmail.com>
2019-09-16 20:21:17 +0300

    lstnew fix

--- Libft/ft_lstnew.c
+++ Libft/ft_lstnew.c
@@ -1,11 +1,11 @@
 #include "libft.h"
+#include <stdlib.h>
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
-	t_list	*list;
+	t_list		*list;
 
-    list = (t_list*)ft_memalloc(sizeof(t_list));
-	if (!list)
+	if (!(list = (t_list *)malloc(sizeof(*list))))
 		return (NULL);
 	if (!content)
 	{
@@ -14,9 +14,9 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	}
 	else
 	{
-		list->content = ft_dup(content, content_size);
-		if (list->content == NULL)
+		if (!(list->content = malloc(content_size)))
 			return (NULL);
+		ft_memcpy(list->content, content, content_size);
 		list->content_size = content_size;
 	}
 	list->next = NULL;
