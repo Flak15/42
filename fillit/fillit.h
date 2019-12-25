896e80da295b351bd5c23eb34efb6769b833afd3
Nymeria Ventress <nventres@mi-q5.21-school.ru>
2019-12-25 14:31:20 +0300

    fixes

--- fillit/fillit.h
+++ fillit/fillit.h
@@ -1,2 +1,7 @@
 #define BUFF_SIZE 8
-char    *read_tet(int fd);
+char			*read_tet(int fd);
+int				check_right(unsigned short tet, int i);
+int				check_left(unsigned short tet, int i);
+int				check_bottom(unsigned short tet, int i);
+void			render_f(unsigned long *tets, int i);
+unsigned long	proccess(unsigned long *field, unsigned long *tets, int index, int f_size);
